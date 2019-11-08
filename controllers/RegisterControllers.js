4cb80be441027e0fbb5f96c0e5932edefa4683a0
crisarevalom <00209718@uca.edu.sv>
2019-11-07 18:20:55 -0600

    Añadiendo delete y update

--- controllers/RegisterControllers.js
+++ controllers/RegisterControllers.js
@@ -1,17 +1,17 @@
 const Register = require("./../models/Register");
 
-const insert = (req, res)=>{
-    const student = new Register(req.body);
-    student.save((error,document) =>{
-        if (error)
+const insert = (req, res) => {
+  const student = new Register(req.body);
+  student.save((error, document) => {
+    if (error)
       res.status(500).json({
         msg: "Hubo error"
       });
-      return res.status(201).json({
-          msg:"creado",
-          register: document
-      })
+    return res.status(201).json({
+      msg: "creado",
+      register: document
     })
+  })
 }
 
 const getRegister = (req, res) => {
@@ -20,28 +20,61 @@ const getRegister = (req, res) => {
       res.status(500).json({
         msg: "Hubo error"
       });
-      return res.status(200).json({
-          msg: "ok",
-          regsiters: documents
-      })
+    return res.status(200).json({
+      msg: "ok",
+      regsiters: documents
+    })
   });
 };
 
-const getOneRegister = (req,res)=>{
-    Register.findById(req.params.id, (error, document) => {
-        if (error)
-          res.status(500).json({
-            msg: "Hubo error"
-          });
-          return res.status(200).json({
-              msg: "ok",
-              regsiter: document
-          })
+const getOneRegister = (req, res) => {
+  Register.findById(req.params.id, (error, document) => {
+    if (error)
+      res.status(500).json({
+        msg: "Hubo error"
       });
+    return res.status(200).json({
+      msg: "ok",
+      regsiter: document
+    })
+  });
+}
+
+
+const update = (req, res) => {
+  let update = {
+    carnet: req.body.carnet,
+    schedule: req.body.schedule,
+    isLate: req.body.isLate
+  };
+  Register.findByIdAndUpdate(req.params.id, update, function (err, old) {
+    if (err) {
+      res.status(500);
+      res.json({ code: 500, err });
+    } else {
+      res.json({ ok: true, old, update });
+    }
+
+  });
+
+}
+
+const deleteR = (req, res) => {
+  Register.deleteOne(req.params.id, function (err, eleminado) {
+    if (err) {
+      res.status(500);
+      res.json({ code: 500, err });
+    } else {
+      res.json({ ok: true, eleminado });
+    }
+  });
 }
 
 module.exports = {
-    getRegister,
-    insert,
-    getOneRegister
+  getRegister,
+  insert,
+  getOneRegister,
+  update,
+  deleteR
+  
 };
--- routes/register.js
+++ routes/register.js
@@ -5,6 +5,8 @@ const registerController = require ("./../controllers/RegisterControllers");
 router.get("/",registerController.getRegister);
 router.get("/:id",registerController.getOneRegister);
 router.post("/",registerController.insert);
+router.post("/",registerController.update);
+router.post("/",registerController.deleteR);
 
 
 module.exports = router;
